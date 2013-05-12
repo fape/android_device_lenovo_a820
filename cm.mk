51cc750dcab8ad6f4ce520ad409a65c68bcedf0b
fape <fape88@gmail.com>
2013-05-12 09:49:48 +0200

    mkvendor.sh: Initial commit of a820

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := a820
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lenovo/a820/device_a820.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a820
+PRODUCT_NAME := cm_a820
+PRODUCT_BRAND := lenovo
+PRODUCT_MODEL := a820
+PRODUCT_MANUFACTURER := lenovo
